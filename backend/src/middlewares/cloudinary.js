635c807f20e3dcf9067223c33427342e521469cc
congthanh1999 <congthanh10021999@gmail.com>
2024-06-24 17:07:03 +0700

    create cloudinary middleware

---
+++ backend/src/middlewares/cloudinary.js
@@ -0,0 +1,44 @@
+const cloudinary = require("cloudinary").v2;
+const { extractFolderName } = require("../utils/helper_function");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true, // Return "https" URLs
+});
+
+const getUrl = async (req, res, next) => {
+  try {
+    if (req.body.imageUrl) {
+      const imagePath = req.body.imageUrl;
+
+      const fileObject = extractFolderName(req.method, req.originalUrl);
+
+      console.log(req.method, req.originalUrl);
+      console.log(fileObject);
+
+      const cloudinaryRes = await cloudinary.uploader.upload(imagePath, {
+        folder: `household-service/${fileObject.baseFolderName}${
+          fileObject.folderName !== fileObject.baseFolderName
+            ? `/${fileObject.folderName}`
+            : ""
+        }`,
+        public_id: fileObject.fileName,
+      });
+
+      if (!cloudinaryRes) {
+        throw new Error("Upload image fail");
+      }
+
+      req.cloudImageUrl = cloudinaryRes.url;
+    }
+  } catch (error) {
+    console.error("Error uploading to Cloudinary:", error);
+    res.status(404).json({ error: error.message });
+  } finally {
+    next();
+  }
+};
+
+module.exports = getUrl;
