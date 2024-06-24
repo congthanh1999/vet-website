e04bfbd663b41163151aa919bbf1c10dd6077066
congthanh1999 <congthanh10021999@gmail.com>
2024-06-24 17:07:53 +0700

    apply cloudinary middleware to instructionsRouter

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 require("express-async-errors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
+
 const postsRouter = require("./controllers/posts");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
-const introductionRouter = require("./controllers/introduction");
+const introductionsRouter = require("./controllers/introductions");
+const introductionArticlesRouter = require("././controllers/introductionArticles");
+
 const middleware = require("./middlewares/middleware");
-const path = require("path");
+const getUrl = require("./middlewares/cloudinary");
 
 const app = express();
 
@@ -23,7 +26,12 @@ app.use(middleware.tokenExtractor);
 app.use("/api/login", loginRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/posts", middleware.userExtractor, postsRouter);
-app.use("/api/introduction", introductionRouter);
+app.use(
+  "/api/introductions",
+  getUrl,
+  introductionsRouter,
+  introductionArticlesRouter
+);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
